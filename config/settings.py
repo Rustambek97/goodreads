72597d45e6d37d78167daab13526fbc2109bdb3d
Rustambek97 <111418072+Rustambek97@users.noreply.github.com>
2024-04-08 23:19:55 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -141,4 +141,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'r.baltayev9997@gmail.com'
-EMAIL_HOST_PASSWORD = 'Rustam123456b'
+EMAIL_HOST_PASSWORD = '*******'
